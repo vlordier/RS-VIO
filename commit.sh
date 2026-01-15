888a68bcf7e0f938edf177673a424a57380f6506
vlordier <vincent.lordier@sanofi.com>
2026-01-15 14:29:13 +0100

    chore: remove commit helper script

--- commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git add .
-git commit -m "feat(loop-closure): add RANSAC verifier and Hamming matcher
-
-- RansacEpipolarVerifier with RANSAC loop (1000 iter default)
-- HammingMatcher for binary descriptors
-- Criterion benchmark suite for matchers/verifiers
-- 19 tests passing, 170 total library tests"
